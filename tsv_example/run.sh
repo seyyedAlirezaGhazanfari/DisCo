5f821a74e97c705e3b30c81b87dd28895e78aee5
SAREGH <59168138+seyyedAlirezaGhazanfari@users.noreply.github.com>
2024-03-10 17:32:59 +0330

    Customized run.sh for our project

--- tsv_example/run.sh
+++ tsv_example/run.sh
@@ -1,1 +1,1 @@
-python ./tsv_example/create_custom_dataset_tsvs.py --split train --root_folder ./tsv_example/toy_dataset --output_folder ./tsv_example/toy_dataset/tsv 
+python ./tsv_example/create_custom_dataset_tsvs.py --split train --root_folder ./tsv_example/dataset --output_folder ./tsv_example/dataset/tsv 
